6f273cc5649d905e86d4bfae422086972ad0f1dd
Manasi Joshi <manasi04joshi@gmail.com>
2022-04-08 05:02:50 +0530

    Hello

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,19 +3,95 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
+router.get('/movies', function(req, res) {
+    let array = ["rand de basnasti", "the shining", "lord of the rings", "bartman begins"]
     console.log(req)
     console.log(req.params.abcd)
-    res.send('dummy response')
+    res.send(array)
 })
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
-});
+
+router.get('/user-profile/:IndexNumber', function(req, res) {
+    
+    req.params.IndexNumber
+       let array = ["Matrix", "the shining", "lord of the rings", "bartman begins"]
+       //  console.log(array[IndexNumber])
+       if(req.params.IndexNumber<array.length){
+           for(var i=0; i<req.params.IndexNumber;i++){
+            }res.send(array[i]);
+            console.log(array[i])
+       }
+        else{
+            res.send("not valid")
+        }
+       
+      
+   })
+    
+
+   
+
+   router.get('/films',function(req,res) {
+    const a = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+
+       res.send(a)
+}) 
+
+router.get('/user-profile/:IndexNumber', function(req, res) {
+    
+    req.params.IndexNumber
+    //    let array = ["Matrix", "the shining", "lord of the rings", "bartman begins"]
+       //  console.log(array[IndexNumber])
+       const array = [ {
+                'id': 1,
+                'name': 'The Shining'
+               }, {
+                'id': 2,
+                'name': 'Incendies'
+               }, {
+                'id': 3,
+                'name': 'Rang de Basanti'
+               }, {
+                'id': 4,
+                'name': 'Finding Nemo'
+               }]
+       if(req.params.IndexNumber<array.length){
+           for(var i=0; i<req.params.IndexNumber;i++){
+            }res.send(array[i]);
+            console.log(array[i])
+       }
+        else{
+            res.send("not valid")
+        }
+       
+      
+   })
+
+
+
+
+
+
+
+// router.get('/test-me', function (req, res) {
+//     console.log('------------------')
+//     console.log(req)
+//     console.log('------------------')
+//     console.log('These are the request query parameters: ', req.query)
+//     res.send('My first ever api!')
+// });
 
 
 
