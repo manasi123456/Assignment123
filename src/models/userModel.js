f061e6eb0cdb991a95d8b8be4377aa9ede08da40
Manasi Joshi <manasi04joshi@gmail.com>
2022-04-12 01:52:53 +0530

    hii

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    bookName: {
+      type: String,
+      unique: true,
+      required: true,
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
+    authorName: String,
+    category: String,
+    year: Number,
+    // mobile: {
+    //     type: String,
+    //     unique: true,
+    //     required: true
+    // },
+    // gender: {
+    //     type: String,
+    //     enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+    // },
     // isIndian: Boolean,
     // parentsInfo: {
     //     motherName: String,
@@ -21,11 +26,11 @@ const userSchema = new mongoose.Schema( {
     //     siblingName: String
     // },
     // cars: [ String  ]
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema) //users
-
-
+module.exports = mongoose.model("User", userSchema); //users
 
 // String, Number
 // Boolean, Object/json, array
